4b28988774b039e5e37d55bd5740af352dfb506c
STOP1187 <stop1187@gmail.com>
2023-03-19 15:11:56 +0300

    add .gitignore

--- cmake-build-debug/search_engine[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "E:/homeWork_C++/QT/search_engine/cmake-build-debug/search_engine[1]_tests.cmake")
-  include("E:/homeWork_C++/QT/search_engine/cmake-build-debug/search_engine[1]_tests.cmake")
-else()
-  add_test(search_engine_NOT_BUILT search_engine_NOT_BUILT)
-endif()
